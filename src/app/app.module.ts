1ee5548536674ec3e54100f8644e7e9ca4cf265e
My2Labs <sking171@ford.com>
2022-05-12 14:54:40 -0500

    Update components to combine.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { HeroContentComponent } from './components/hero-content/hero-content.com
 import { MainComponent } from './components/main/main.component';
 import { ProgressComponent } from './components/progress/progress.component';
 import { WineComponent } from './components/wine/wine.component';
-import { WineNewComponent } from './components/wine-new/wine-new.component';
 import { WineDrinkingComponent } from './components/wine-drinking/wine-drinking.component';
 import { DrinkAgainComponent } from './components/drink-again/drink-again.component';
 import { KeepDrinkingComponent } from './components/keep-drinking/keep-drinking.component';
@@ -31,17 +30,13 @@ import { HeroImageComponent } from './components/hero-image/hero-image.component
     MainComponent,
     ProgressComponent,
     WineComponent,
-    WineNewComponent,
     WineDrinkingComponent,
     DrinkAgainComponent,
     KeepDrinkingComponent,
     HeroImageComponent,
   ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
