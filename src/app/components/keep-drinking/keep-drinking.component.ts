498afc7ba889846cd603f2f9431d1b7c372892aa
My2Labs <sking171@ford.com>
2022-05-18 06:23:12 -0500

    Fixed iteration.

--- src/app/components/keep-drinking/keep-drinking.component.ts
+++ src/app/components/keep-drinking/keep-drinking.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-keep-drinking',
   templateUrl: './keep-drinking.component.html',
-  styleUrls: ['./keep-drinking.component.css']
+  styleUrls: ['./keep-drinking.component.css'],
 })
 export class KeepDrinkingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
